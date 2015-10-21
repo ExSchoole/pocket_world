a79790d9518f1c9208cb538eebb29861ea43a1da
cat_eat_dog12 <12fthofmay@gmail.com>
2015-10-21 18:06:34 +0300

    import jpa config to root spring config

--- src/main/java/org/exschool/pocketworld/config/SpringRootConfig.java
+++ src/main/java/org/exschool/pocketworld/config/SpringRootConfig.java
@@ -2,9 +2,11 @@ package org.exschool.pocketworld.config;
 
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
 
 @Configuration
 @ComponentScan({"org.exschool.pocketworld.dao", "org.exschool.pocketworld.service" })
+@Import(PersistenceJPAConfig.class)
 public class SpringRootConfig {
 
 }
