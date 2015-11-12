3674f2ffe38820703c7c204c9c48b3b668b89975
ASkandy <lskachkovl@gmail.com>
2015-11-12 15:52:49 +0200

    fix: building model

---
+++ common/src/main/java/org/exschool/pocketworld/resource/model/Resource.java
@@ -0,0 +1,48 @@
+package org.exschool.pocketworld.resource.model;
+
+import javax.persistence.Column;
+import javax.persistence.Embeddable;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+
+@Embeddable
+public class Resource {
+
+	public Resource() {}
+
+	
+
+	@Enumerated(EnumType.STRING)
+	@Column(name = "resource_type")
+	protected ResourceType resourceType;
+
+	@Column(name = "amount")
+	protected int amount;
+
+	public ResourceType getResourceType() {
+		return resourceType;
+	}
+
+	public void setResourceType(ResourceType resourceType) {
+		this.resourceType = resourceType;
+	}
+
+	public int getAmount() {
+		return amount;
+	}
+
+	public void setAmount(int count) {
+		this.amount = count;
+	}
+	
+	public Resource(ResourceType resourceType, int amount) {
+		this.resourceType = resourceType;
+		this.amount = amount;
+	}
+	@Override
+	public String toString() {
+		return "Resource [resourceType=" + resourceType + ", amount=" + amount
+				+ "]";
+	}
+
+}
