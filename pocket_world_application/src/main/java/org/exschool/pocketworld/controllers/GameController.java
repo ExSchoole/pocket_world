c9ac545e644608770a2ed2a25026239b3929a038
l-o-b-s-t-e-r <vladislav-loboda@mail.ru>
2015-11-14 01:43:22 +0200

    I'v added mapping of buildings in the city center.

--- pocket_world_application/src/main/java/org/exschool/pocketworld/controllers/GameController.java
+++ pocket_world_application/src/main/java/org/exschool/pocketworld/controllers/GameController.java
@@ -1,5 +1,10 @@
 package org.exschool.pocketworld.controllers;
 
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+import java.util.Random;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
@@ -8,7 +13,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
-import java.util.Map;
+import Resource.Resource;
+
 
 @Controller
 public class GameController {
@@ -32,7 +38,31 @@ public class GameController {
 			@RequestParam Map<String, String> allRequestParams, Model model) {
 		logger.info("Requested params:" + allRequestParams);
 		logger.info("Out:"+ model);
+		
+		Random r = new Random();
+		Map<Integer,String> builds = new HashMap<Integer,String>(); //<position,type>
+		
+		for (int i=0; i<12; i++)
+			builds.put(r.nextInt(12)+1, findHouse(r.nextInt(4)+1)); //заполн€ем фиктивными данными
+
+		model.addAttribute("BuildObj", builds);
+						
 		return "city_center";
+	}
+	
+	private String findHouse(int type){ //предполагаетс€ что в Ѕƒ типа здани€ ханитс€ как число
+		                                //получаем путь к к картинке котора€ соответствует типа здани€
+		String houseURL;
+		
+		switch(type){
+		case 1: houseURL="images/house34.png"; break;
+		case 2: houseURL="images/building78.png"; break;
+		case 3: houseURL="images/factory6.png"; break;
+		case 4: houseURL="images/farm.png"; break;
+		
+		default : houseURL="images/museum39.png"; break;
+		}
+		return houseURL;
 	}
 
 }
