30119621ae597347a465add485c61587ce756fdf
cat_eat_dog12 <12fthofmay@gmail.com>
2015-11-17 18:28:21 +0200

    tests config + simple test (not finished)

--- pocket_world_application/src/test/java/org/exschool/pocketworld/player/PlayerServiceTest.java
+++ pocket_world_application/src/test/java/org/exschool/pocketworld/player/PlayerServiceTest.java
@@ -1,118 +1,55 @@
 package org.exschool.pocketworld.player;
 
-import static org.junit.Assert.assertTrue;
-import static org.junit.Assert.*;
-import static org.junit.Assert.fail;
-import static org.mockito.Mockito.*;
-
-import java.util.ArrayList;
-import java.util.Iterator;
-import java.util.List;
-
+import org.exschool.pocketworld.config.TestSpringConfig;
 import org.exschool.pocketworld.dao.Dao;
 import org.exschool.pocketworld.player.model.Player;
-import org.exschool.pocketworld.player.service.PlayerService;
 import org.exschool.pocketworld.player.service.PlayerServiceImpl;
-import org.junit.After;
-import org.junit.AfterClass;
-import org.junit.Before;
-import org.junit.BeforeClass;
-import org.junit.Test;
+import org.junit.*;
 import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
-import org.mockito.runners.MockitoJUnitRunner;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.orm.hibernate4.HibernateTemplate;
+import org.mockito.MockitoAnnotations;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.support.AnnotationConfigContextLoader;
+
+import java.util.Iterator;
+import java.util.List;
+
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.fail;
+import static org.mockito.Mockito.when;
 
 
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(loader = AnnotationConfigContextLoader.class, classes = TestSpringConfig.class)
+public class PlayerServiceTest {
 
+    @Mock
+    Dao dao;
 
-public class PlayerServiceTest  {
-	
+    @InjectMocks
+    PlayerServiceImpl playerService;
 
-	
-    private static PlayerServiceImpl playerService;
-
-	List<Player> players;
-	Player player;
-	Player player1;
-	Player player2;
-	
-	@BeforeClass
-    public static void setUpBeforeClass() throws Exception {
+    @Before
+    public void before() {
+        MockitoAnnotations.initMocks(this);
     }
 
-    @AfterClass
-    public static void tearDownAfterClass() throws Exception {
+    @Test
+    public void testSave() {
+        Player player = new Player();
+        player.setPlayerId(1L);
+        player.setLogin("player-login");
+        playerService.savePlayer(player);
+        assert playerService.getPlayerByLogin("player-login").equals(player);
     }
-    
-	@Before
-	   public void before(){
-	      this.playerService = new PlayerServiceImpl();
-	      this.playerService.setDao(mock(Dao.class));
-	      
-	      
-	      players= new ArrayList<>();
-	      
-	      Player player = new Player();
-	      player.setLogin("test");
-	      playerService.savePlayer(player);
-	      players.add(player);
-			
-	      Player player1 = new Player();
-	      player.setLogin("test");
-	      playerService.savePlayer(player1);
-	      players.add(player1);
-			
-	      Player player2 = new Player();
-	      player.setLogin("test1");
-	      playerService.savePlayer(player2);
-	      players.add(player2);
-			
-	      
-	   }
-	@After
-    public void tearDown() throws Exception {
+
+
+    @Test
+    public void testGetByLogin() {
     }
-	
-	@Test
-	public void testSave()
-	{
-		Iterator<Player> itr= players.iterator();
-		while(itr.hasNext())
-		{
-			try {
-					playerService.savePlayer(itr.next());
-		     	}
-			catch (Exception e) {
-		    	  fail("Exeptiton!");
-		      	}
-		}
-	}
-	
-	
-	@Test
-	public void testGetByLogin()
-	{
-		Iterator<Player> itr= players.iterator();
-		while(itr.hasNext())
-		{
-			try {
-					when(playerService.getPlayerByLogin(itr.next().getLogin())).thenReturn( player);
-					assertTrue(true);
-		     	}
-			catch (Exception e) {
-		    	  	fail("Exeptiton!");
-		      	}
-		}
-		
-	}
-	
+
 
 }
 
