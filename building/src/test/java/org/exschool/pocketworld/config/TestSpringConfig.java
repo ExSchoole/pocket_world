6f4041fee63d2323b38726a4154e94feba89a00b
ASkandy <lskachkovl@gmail.com>
2015-11-18 22:02:11 +0200

    add annotation in TestSpringConfig

--- building/src/test/java/org/exschool/pocketworld/config/TestSpringConfig.java
+++ building/src/test/java/org/exschool/pocketworld/config/TestSpringConfig.java
@@ -12,11 +12,13 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.orm.hibernate5.HibernateTransactionManager;
 import org.springframework.orm.hibernate5.LocalSessionFactoryBean;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 import javax.sql.DataSource;
 import java.util.Properties;
 
 @Configuration
+@EnableTransactionManagement
 @ComponentScan({"org.exschool.pocketworld.dao", "org.exschool.pocketworld.building" })
 public class TestSpringConfig {
 
