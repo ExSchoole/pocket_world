0d9a277849c2dafe987d75a30714393253322939
ASkandy <lskachkovl@gmail.com>
2015-11-18 15:34:05 +0200

    add test config

--- building/src/main/java/org/exschool/pocketworld/building/model/Building.java
+++ building/src/main/java/org/exschool/pocketworld/building/model/Building.java
@@ -31,7 +31,13 @@ public class Building {
 	private Long cityId;
 
 
+	public Long getId() {
+		return id;
+	}
 
+	public void setId(Long id) {
+		this.id = id;
+	}
 
 	public int getPosition() {
 		return position;
@@ -58,5 +64,31 @@ public class Building {
 
 	public void setCityId(Long cityId) {
 		this.cityId = cityId;
+	}
+
+	@Override
+	public boolean equals(Object o) {
+		if (this == o) return true;
+		if (o == null || getClass() != o.getClass()) return false;
+
+		Building building = (Building) o;
+
+		if (level != building.level) return false;
+		if (position != building.position) return false;
+		if (buildingType != building.buildingType) return false;
+		if (cityId != null ? !cityId.equals(building.cityId) : building.cityId != null) return false;
+		if (id != null ? !id.equals(building.id) : building.id != null) return false;
+
+		return true;
+	}
+
+	@Override
+	public int hashCode() {
+		int result = id != null ? id.hashCode() : 0;
+		result = 31 * result + (buildingType != null ? buildingType.hashCode() : 0);
+		result = 31 * result + level;
+		result = 31 * result + position;
+		result = 31 * result + (cityId != null ? cityId.hashCode() : 0);
+		return result;
 	}
 }
--- building/src/test/java/org/exschool/pocketworld/building/service/BuildingServiceTest.java
+++ building/src/test/java/org/exschool/pocketworld/building/service/BuildingServiceTest.java
@@ -6,24 +6,42 @@ import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
 import org.mockito.Mockito;
+import org.mockito.MockitoAnnotations;
 import org.mockito.runners.MockitoJUnitRunner;
+import org.exschool.pocketworld.building.service.TestSpringConfig;
+import org.springframework.test.context.support.AnnotationConfigContextLoader;
+import org.junit.*;
+
+
+
+
+import java.util.Arrays;
+import java.util.LinkedList;
 
 @RunWith(MockitoJUnitRunner.class)
+@ContextConfiguration(loader = AnnotationConfigContextLoader.class, classes = TestSpringConfig.class)
 public class BuildingServiceTest {
+    @InjectMocks
     private BuildingServiceImpl service;
+    @Mock
     private Dao dao;
     private Building building1, building2;
 
     @Before
     public void init() {
+        MockitoAnnotations.initMocks(this);
         service = new BuildingServiceImpl();
         dao = Mockito.mock(Dao.class);
         service.setDao(dao);
         building1 = new Building();
+        building1.setId(1L);
         building1.setLevel(1);
         building1.setPosition(1);
         building1.setCityId(1L);
+        building1.setId(1L);
     }
 
     @Test
@@ -32,5 +50,28 @@ public class BuildingServiceTest {
         Assert.assertEquals(building1, service.get(1L));
         Assert.assertNull(service.get(0L));
     }
+
+    @Test
+    public void getAllTest() {
+        LinkedList<Building> allBuildings = new LinkedList<Building>(Arrays.asList(building1));
+        Mockito.when(dao.all(Building.class)).thenReturn(allBuildings);
+        Assert.assertEquals(allBuildings, service.allBuildings());
+        Mockito.when (dao.all(Building.class)).thenReturn(null);
+        Assert.assertNull(service.allBuildings());
+    }
+
+    @Test
+    public void saveTest()
+    {
+        /*playerService.savePlayer(player);
+        assert playerService.getPlayerByLogin("player-login").equals(player);
+*/
+        service.save(building1);
+        assert service.get(1L).equals(building1);
+
+
+    }
+
+
 
 }
