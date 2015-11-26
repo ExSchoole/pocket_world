e51f51f80e56a654f8451f2690cabe3b6566e69b
cat_eat_dog12 <12fthofmay@gmail.com>
2015-11-26 15:24:30 +0200

    remove unused import

--- pocket_world_application/src/main/java/org/exschool/pocketworld/city/center/dto/CityCenterDto.java
+++ pocket_world_application/src/main/java/org/exschool/pocketworld/city/center/dto/CityCenterDto.java
@@ -2,7 +2,6 @@ package org.exschool.pocketworld.city.center.dto;
 
 import org.exschool.pocketworld.building.Building;
 import org.exschool.pocketworld.resource.ResourceDto;
-import java.util.Stack;
 
 import java.util.Map;
 
