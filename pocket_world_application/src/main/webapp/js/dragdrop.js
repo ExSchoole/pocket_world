bd675a38bc8c20e70f75888ffd317f049382aa2b
l-o-b-s-t-e-r <vladislav-loboda@mail.ru>
2015-12-10 18:12:45 +0200

    I fixed Drag&Drop...I hope

--- pocket_world_application/src/main/webapp/js/dragdrop.js
+++ pocket_world_application/src/main/webapp/js/dragdrop.js
@@ -1,7 +1,7 @@
 function dragDrop(config,type,position) {
 	$.ajax({          
 		   type: 'POST',
-		   url: config.buildUrl+"/city/center/addBuilding",
+		   url: config.buildUrl,
 		   data : { type: type, position: position},
 		   success : function(data) {
 			   			console.log("SUCCESS");
