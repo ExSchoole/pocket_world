ee59a18899525c5752a92a360688b1d0b8971983
cat_eat_dog12 <kgavrylchenko@luxoft.com>
2015-12-16 11:00:23 +0200

    renamed class

--- pocket_world_application/src/main/java/org/exschool/pocketworld/controllers/MainController.java
+++ pocket_world_application/src/main/java/org/exschool/pocketworld/controllers/MainController.java
@@ -1,15 +1,15 @@
-package org.exschool.pocketworld.controllers.city.center;
+package org.exschool.pocketworld.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+
 @Controller
 @RequestMapping("/")
-public class CityController {
+public class MainController {
 
-	@RequestMapping(value = "/", method = RequestMethod.GET)
+    @RequestMapping(value = "/", method = RequestMethod.GET)
     public String showCity() {
-        
         return "redirect:/city/center/";
     }
 
