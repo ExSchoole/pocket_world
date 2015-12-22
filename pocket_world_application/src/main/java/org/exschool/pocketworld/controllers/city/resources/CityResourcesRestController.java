bc4829d76e2956de94c9e8ac4ad24b69d3ecc7f8
Vitaliy G <gretchko.v@gmail.com>
2015-12-22 21:25:15 +0200

    fix tests

--- pocket_world_application/src/main/java/org/exschool/pocketworld/controllers/city/resources/CityResourcesRestController.java
+++ pocket_world_application/src/main/java/org/exschool/pocketworld/controllers/city/resources/CityResourcesRestController.java
@@ -1,10 +1,17 @@
 package org.exschool.pocketworld.controllers.city.resources;
 
+import org.exschool.pocketworld.city.model.City;
+import org.exschool.pocketworld.city.service.CityService;
 import org.exschool.pocketworld.dto.PositionOfBulding;
+import org.exschool.pocketworld.player.builder.PlayerBuilder;
+import org.exschool.pocketworld.player.model.Player;
+import org.exschool.pocketworld.player.model.PlayerResources;
+import org.exschool.pocketworld.player.service.PlayerService;
 import org.exschool.pocketworld.resource.building.model.ResourceBuilding;
 import org.exschool.pocketworld.resource.building.service.ResourceBuildingService;
 import org.exschool.pocketworld.resource.model.ResourceType;
 import org.exschool.pocketworld.util.builder.ResourceBuildingBuilder;
+import org.exschool.pocketworld.util.builder.UserCityBuilder;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -21,6 +28,12 @@ public class CityResourcesRestController {
     @Autowired
     private ResourceBuildingService resourceBuildingService;
 
+    @Autowired
+    private CityService cityService;;
+
+    @Autowired
+    private PlayerService playerService;
+
     @RequestMapping(value="/types",method=RequestMethod.GET)
     public List<String> getResourceBuildingsTypesList(){
         LOGGER.info("getResourceBuildingsList is invoked");
@@ -30,15 +43,25 @@ public class CityResourcesRestController {
     @RequestMapping(value="/buildings", method=RequestMethod.POST)
     @ResponseStatus(HttpStatus.CREATED)
     public void createResourceBuilding(@RequestBody PositionOfBulding positionOfBulding){
-        final Integer INITIAL_BUILDING_LEVEL = 0;
         LOGGER.info("RequestBody:" + positionOfBulding);
+        final Integer INITIAL_BUILDING_LEVEL = 0;
+
+        // -- temporary
+        String playerLogin = "login-1";
+        Player player = playerService.getPlayerByLogin(playerLogin);
+        City city = cityService.getCityByPlayerId(player.getId());
+        // -- end temporary
+
         ResourceType resourceType = ResourceType.valueOf(positionOfBulding.getType().toUpperCase());
+
 
         ResourceBuilding resourceBuilding = ResourceBuildingBuilder.builder()
                                .buildingType(resourceType)
+                               .cityId(city.getId())
                                .level(INITIAL_BUILDING_LEVEL)
                                .position(positionOfBulding.getPosition())
                                .build();
+
         resourceBuildingService.save(resourceBuilding);
     }
 }
--- pocket_world_application/src/test/java/org/exschool/pocketworld/city/resources/rest/service/CityResourcesRestControllerTest.java
+++ pocket_world_application/src/test/java/org/exschool/pocketworld/city/resources/rest/service/CityResourcesRestControllerTest.java
@@ -2,13 +2,24 @@ package org.exschool.pocketworld.city.resources.rest.service;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import org.exschool.pocketworld.city.service.CityService;
 import org.exschool.pocketworld.config.SpringWebConfig;
 import org.exschool.pocketworld.config.TestSpringRootConfig;
+import org.exschool.pocketworld.controllers.city.resources.CityResourcesRestController;
 import org.exschool.pocketworld.dto.PositionOfBulding;
+import org.exschool.pocketworld.player.builder.PlayerBuilder;
+import org.exschool.pocketworld.player.model.PlayerResources;
+import org.exschool.pocketworld.player.service.PlayerService;
+import org.exschool.pocketworld.resource.building.model.ResourceBuilding;
+import org.exschool.pocketworld.resource.building.service.ResourceBuildingService;
+import org.exschool.pocketworld.util.builder.UserCityBuilder;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.MockitoAnnotations;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
@@ -16,11 +27,11 @@ import org.springframework.test.context.web.AnnotationConfigWebContextLoader;
 import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
-import org.springframework.web.context.WebApplicationContext;
 
 import java.nio.charset.Charset;
 
 import static org.hamcrest.CoreMatchers.hasItems;
+import static org.mockito.Mockito.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
@@ -35,12 +46,22 @@ public class CityResourcesRestControllerTest {
     private final String ROOT = "/v1/city/resources";
     private MockMvc mockMvc;
 
-    @Autowired
-    private WebApplicationContext wac;
+    @Mock
+    private CityService cityService;
+
+    @Mock
+    private PlayerService playerService;
+
+    @Mock
+    private ResourceBuildingService resourceBuildingService;
+
+    @InjectMocks
+    CityResourcesRestController cityResourcesRestController;
 
     @Before
     public void setup(){
-        this.mockMvc = MockMvcBuilders.webAppContextSetup(this.wac).build();
+        MockitoAnnotations.initMocks(this);
+        this.mockMvc = MockMvcBuilders.standaloneSetup(cityResourcesRestController).build();
     }
 
     @Test
@@ -54,8 +75,8 @@ public class CityResourcesRestControllerTest {
 
     @Test
     public void test_build_resource_building() throws Exception {
+        configureMocks();
         PositionOfBulding positionOfBulding = new PositionOfBulding(10, "gold");
-
         String jsonInString = positionOfBuildingToJson(positionOfBulding);
 
         mockMvc.perform(
@@ -63,10 +84,31 @@ public class CityResourcesRestControllerTest {
                         .contentType(MediaType.APPLICATION_JSON)
                         .content(jsonInString))
                 .andExpect(status().isCreated());
+
+        verify(resourceBuildingService,times(1)).save(Mockito.any(ResourceBuilding.class));
     }
 
     private String positionOfBuildingToJson(PositionOfBulding positionOfBulding) throws JsonProcessingException {
         ObjectMapper mapper = new ObjectMapper();
         return mapper.writeValueAsString(positionOfBulding);
+    }
+
+    private void configureMocks(){
+        String playerLogin = "login-1";
+        Long playerId = 1L;
+
+        when(playerService.getPlayerByLogin(playerLogin))
+                .thenReturn(PlayerBuilder
+                        .builder()
+                        .playerId(1L)
+                        .login(playerLogin)
+                        .playerResources(new PlayerResources(1,1,1,1))
+                        .build());
+        when(cityService.getCityByPlayerId(playerId))
+                .thenReturn(UserCityBuilder
+                        .builder()
+                        .name("Test")
+                        .playerId(playerId)
+                        .build());
     }
 }
