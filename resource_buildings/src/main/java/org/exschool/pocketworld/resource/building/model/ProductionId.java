c883096601d7ed96e4aa838cd4db6d4baf248446
l-o-b-s-t-e-r <vladislav-loboda@mail.ru>
2016-01-21 14:53:46 +0200

    fill tests

--- resource_buildings/src/main/java/org/exschool/pocketworld/resource/building/model/ProductionId.java
+++ resource_buildings/src/main/java/org/exschool/pocketworld/resource/building/model/ProductionId.java
@@ -14,7 +14,7 @@ import java.io.Serializable;
 @Embeddable
 public class ProductionId implements Serializable {
 
-    @Enumerated(EnumType.STRING)
+	@Enumerated(EnumType.STRING)
     @Column(name = "resource_type")
     private ResourceType resourceType;
 
@@ -44,4 +44,32 @@ public class ProductionId implements Serializable {
     public void setLevel(int level) {
         this.level = level;
     }
+    
+    @Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((level == null) ? 0 : level.hashCode());
+		result = prime * result + ((resourceType == null) ? 0 : resourceType.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		ProductionId other = (ProductionId) obj;
+		if (level == null) {
+			if (other.level != null)
+				return false;
+		} else if (!level.equals(other.level))
+			return false;
+		if (resourceType != other.resourceType)
+			return false;
+		return true;
+	}
 }
--- resource_buildings/src/test/java/org/exschool/pocketworld/resource/building/service/ResourceBuildingBootstrap.java
+++ resource_buildings/src/test/java/org/exschool/pocketworld/resource/building/service/ResourceBuildingBootstrap.java
@@ -1,5 +1,9 @@
 package org.exschool.pocketworld.resource.building.service;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 import org.exschool.pocketworld.dao.Dao;
 import org.exschool.pocketworld.resource.building.model.ResourceBuilding;
 import org.exschool.pocketworld.resource.model.ResourceType;
@@ -7,8 +11,6 @@ import org.exschool.pocketworld.util.builder.ResourceBuildingBuilder;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-
-import java.util.Arrays;
 
 /**
  * Created by manoylo on 20.11.15.
@@ -19,18 +21,37 @@ public class ResourceBuildingBootstrap {
     @Autowired
     Dao dao;
 
+    private List<ResourceBuilding> buildings;
     private volatile boolean bootstraped = false;
 
     public void fillDatabase() {
         if (bootstraped) return;
+        buildings = new ArrayList<>();
+        
         ResourceBuilding resourceBuilding1 = ResourceBuildingBuilder.builder().buildingType(ResourceType.GOLD).level(1).position(1).cityId(1L).build();
+        buildings.add(resourceBuilding1);
+        
         ResourceBuilding resourceBuilding2 = ResourceBuildingBuilder.builder().buildingType(ResourceType.TIMBER).level(1).position(2).cityId(2L).build();
+        buildings.add(resourceBuilding2);
+        
         ResourceBuilding resourceBuilding3 = ResourceBuildingBuilder.builder().buildingType(ResourceType.TIMBER).level(1).position(3).cityId(3L).build();
+        buildings.add(resourceBuilding3);
+        
         ResourceBuilding resourceBuilding4 = ResourceBuildingBuilder.builder().buildingType(ResourceType.CLAY).level(1).position(1).cityId(5L).build();
+        buildings.add(resourceBuilding4);
+        
         ResourceBuilding resourceBuilding5 = ResourceBuildingBuilder.builder().buildingType(ResourceType.TIMBER).level(3).position(2).cityId(5L).build();
+        buildings.add(resourceBuilding5);
+        
         ResourceBuilding resourceBuilding6 = ResourceBuildingBuilder.builder().buildingType(ResourceType.CORN).level(4).position(3).cityId(5L).build();
+        buildings.add(resourceBuilding6);
+        
         dao.saveAll(Arrays.asList(resourceBuilding1, resourceBuilding2, resourceBuilding3,resourceBuilding4,resourceBuilding5,resourceBuilding6));
         bootstraped = true;
+    }
+    
+    public List<ResourceBuilding> getBuildings(){
+        return buildings;
     }
 
 }
