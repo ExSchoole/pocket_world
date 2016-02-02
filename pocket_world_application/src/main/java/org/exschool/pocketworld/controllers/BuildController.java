8360313d8998be814016bbf24356df7b5d0523c0
kgavrylchenko <12fthofmay@gmail.com>
2016-02-02 22:37:38 +0200

    added clocl image on div with building if timers contains it position

--- pocket_world_application/src/main/java/org/exschool/pocketworld/controllers/BuildController.java
+++ pocket_world_application/src/main/java/org/exschool/pocketworld/controllers/BuildController.java
@@ -5,7 +5,9 @@ import com.google.common.base.Predicate;
 import com.google.common.collect.Collections2;
 import org.exschool.pocketworld.buildQueue.model.BuildQueueRecord;
 import org.exschool.pocketworld.buildQueue.model.Status;
+import org.exschool.pocketworld.buildQueue.model.Type;
 import org.exschool.pocketworld.buildQueue.service.BuildQueueService;
+import org.exschool.pocketworld.building.model.BuildingType;
 import org.exschool.pocketworld.player.service.PlayerService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -13,6 +15,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import java.util.Arrays;
 import java.util.Collection;
 import java.util.List;
 
@@ -38,12 +41,15 @@ public class BuildController {
                 return input.getStatus() == Status.QUEUED && input.getBuildEnd().isAfterNow();
             }
         });
-        return Collections2.transform(activeRecords, new Function<BuildQueueRecord, Timer>() {
+        /*return*/ Collections2.transform(activeRecords, new Function<BuildQueueRecord, Timer>() {
             @Override
             public Timer apply(BuildQueueRecord input) {
                 return Timer.create().type(input.getType().name().toLowerCase()).buildEndInSeconds(1000L).position(3).get();
             }
         });
+        return Arrays.asList(Timer.create().type(Type.BUILDING.name().toLowerCase()).buildEndInSeconds(1000L).position(3).get(),
+                Timer.create().type(Type.BUILDING.name().toLowerCase()).buildEndInSeconds(1000L).position(3).get(),
+        Timer.create().type(Type.RESOURCE_BUILDING.name().toLowerCase()).buildEndInSeconds(1000L).position(3).get());
     }
 
     private static class Timer {
@@ -56,6 +62,18 @@ public class BuildController {
             this.type = type;
             this.buildEndInSeconds = buildEndInSeconds;
             this.position = position;
+        }
+
+        public String getType() {
+            return type;
+        }
+
+        public long getBuildEndInSeconds() {
+            return buildEndInSeconds;
+        }
+
+        public int getPosition() {
+            return position;
         }
 
         public static Builder create() {
