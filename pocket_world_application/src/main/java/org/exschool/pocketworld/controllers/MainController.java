c697fa58896ffae38b5fc2684d9837f0696fd4fd
Vlad <vladislav-loboda@mail.ru>
2016-03-18 21:49:26 +0200

    checkstyle

--- pocket_world_application/src/main/java/org/exschool/pocketworld/controllers/MainController.java
+++ pocket_world_application/src/main/java/org/exschool/pocketworld/controllers/MainController.java
@@ -14,7 +14,6 @@ import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.web.authentication.logout.SecurityContextLogoutHandler;
 import org.springframework.stereotype.Controller;
-import org.springframework.util.comparator.BooleanComparator;
 import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletRequest;
